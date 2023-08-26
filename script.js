7b52885f354b63f18cc352214a3b2fbdb039f15a
AteGGz <aaryavbehl47@gmail.com>
2023-08-26 10:02:33 +0530

    sad

--- script.js
+++ script.js
@@ -128,16 +128,3 @@ function changeColor() {
 
 setInterval(changeColor, 1000);
 
-const cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-})
-
-document.addEventListener('click', () => {
-    cursor.classList.add("expand");
-
-    setTimeout(() => {
-        cursor.classList.remove("expand");
-    }, 500)
-})
